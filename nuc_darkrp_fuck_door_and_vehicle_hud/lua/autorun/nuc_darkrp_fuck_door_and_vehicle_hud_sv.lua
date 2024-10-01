d024f17f06c8e72caa06ff43d8f0a00cbb70903d
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 11:39:38 +0200

    Add files via upload

---
+++ nuc_darkrp_fuck_door_and_vehicle_hud/lua/autorun/nuc_darkrp_fuck_door_and_vehicle_hud_sv.lua
@@ -0,0 +1,6 @@
+--------------------------------------------
+--*/ NUC JOIN THE BATTLE
+--------------------------------------------
+MsgC (Color(255, 255, 255), [[                                                                                                              
+= NUC - DarkRP Fuck Door And Vehicle HUD loaded!
+]])
